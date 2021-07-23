9a26cc20e3213d640556931889cfa38dd4266a1d
Louie <louie.chen@baifu-tech.net>
2021-07-23 16:09:14 +0800

    home page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,113 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <el-col :span="20" :offset="2">
+            <div id="nav">
+                <el-row>
+                    <el-col :span="12">
+                        <div style="float: left;">
+                            <img alt="Vue logo" src="@/assets/logo.png" />
+                        </div>
+                    </el-col>
+                    <el-col :span="12">
+                        <div class="date">
+                            2021-07-23 15:05 星期五
+                        </div>
+                    </el-col>
+                </el-row>
+            </div>
+
+            <el-menu
+                :default-active="activeIndex"
+                class="el-menu-demo"
+                mode="horizontal"
+                @select="handleSelect"
+                style="margin-bottom: 20px;"
+            >
+                <el-row>
+                    <el-col :span="4">
+                        <el-menu-item index="1" @click="$router.push('/')">
+                            首頁
+                        </el-menu-item>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-menu-item index="2" @click="$router.push('/about')">
+                            開獎公告
+                        </el-menu-item>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-menu-item index="3" @click="$router.push('/about')">
+                            開獎視頻
+                        </el-menu-item>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-menu-item index="4" @click="$router.push('/about')">
+                            FAQ
+                        </el-menu-item>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-menu-item index="5" @click="$router.push('/about')">
+                            關於我們
+                        </el-menu-item>
+                    </el-col>
+                    <el-col :span="4">
+                        <el-menu-item index="6" @click="$router.push('/about')">
+                            聯繫我們
+                        </el-menu-item>
+                    </el-col>
+                </el-row>
+            </el-menu>
+
+            <router-view />
+        </el-col>
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeIndex: "1",
+            activeIndex2: "1",
+        };
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        },
+    },
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    width: 100%;
+    min-width: 1170px;
+    height: 140px;
+}
+
+.date {
+    float: right;
+    line-height: 110px;
+    font-size: 18px;
+    color: #a4a4a4;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
+    border-bottom: 1px solid #dadada;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+    font-weight: bold;
+    color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home" style="margin-top: 20px;">
+        <!--<HelloWorld msg="Welcome to Your Vue.js App" />-->
+
+        <div class="block">
+            <el-carousel height="423px">
+                <el-carousel-item v-for="item in imgUrl" :key="item">
+                    <img :src="item" alt="" style="width:100%" />
+                </el-carousel-item>
+            </el-carousel>
+        </div>
+
+        <div class="home_box">
+            <div class="box_title amlhc">
+                <span class="title">澳門六合彩</span>
+                <span class="nextTime"
+                    >第2021204期截止時間：2021-07-23 21:15:00</span
+                >
+            </div>
+            <div>
+                <el-row>
+                    <el-col :span="15">123123</el-col>
+                    <el-col :span="9">412</el-col>
+                </el-row>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    name: "Home",
+    components: {},
+    data() {
+        return {
+            imgUrl: [
+                "https://mcjccdn-qq.goluosi.com/macaujc/pc/img/swiper1.jpg",
+                "https://mcjccdn-qq.goluosi.com/macaujc/pc/img/swiper3.jpg",
+                "https://mcjccdn-qq.goluosi.com/macaujc/pc/img/swiper4.jpg",
+            ],
+        };
+    },
+};
 </script>
+
+<style>
+.home_box {
+    margin-top: 20px;
+    box-shadow: 0 1px 4px #c8c8c8;
+    border: 1px solid #dadada;
+    border-radius: 4px;
+    background-color: #fff;
+}
+
+.box_title .nextTime {
+    font-size: 14px;
+    float: right;
+}
+
+.box_title .title {
+    font-size: 20px;
+    float: left;
+}
+.box_title {
+    height: 40px;
+    line-height: 40px;
+    padding: 0 20px;
+}
+.amlhc {
+    background-color: #ffc9c9;
+    border: 1px solid #d80011;
+    color: #d80011;
+}
+</style>
